20a2133af29d4927b1ff2af6264668a13dc02625
yunchen <1677693299@qq.com>
2022-03-20 09:26:36 +0800

    test

--- shell_sort.py
+++ shell_sort.py
@@ -1,3 +1,24 @@
-def get_times(length):
-    return length // 2
+from cac_fuc.sort import random_n
 
+
+def shell_sort(li):
+    group = len(li) // 2
+    while group >= 1:
+        insert_sort_gap(li, group)
+        group //= 2
+    print(li)
+
+
+def insert_sort_gap(li, gap):  # 相当于摸牌，从右开始比，到比自己小停止
+    for i in range(gap, len(li)):
+        tmp = li[i]
+        j = int(i) - gap  # 手里牌坐标
+
+        while li[j] > tmp and j >= 0:
+            li[j + gap] = li[j]
+            j -= gap
+        li[j + gap] = tmp
+    # print(li)
+
+
+shell_sort(random_n())
